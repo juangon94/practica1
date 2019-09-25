cc22889af343d0044ec24dd45c072d5ac96f4223
juangon94 <55006389+juangon94@users.noreply.github.com>
2019-09-25 15:06:14 -0400

    Update practica8.php

--- practica8.php
+++ practica8.php
@@ -4,10 +4,10 @@ $hora =$_GET["hora"];
 if ($hora > 8 && $hora <=12) {
     echo "buen dia $a, espero que tengas un buen dia.";
 }
-elseif ($hora > 12 && $hora <=18) {
+elseif ($hora > 12 || $hora ==18) {
     echo "buen dia $a, espero que hayas tenido una buena manana y una buena tarde. ";
 }
-elseif ($hora > 12 || $hora==24) {
+elseif ($hora > 18 && $hora<=24) {
     echo "Buen dia $a, espero que hayas tenido una buena manana una buena tarde y que tengas una buena noche.";
 }
 
